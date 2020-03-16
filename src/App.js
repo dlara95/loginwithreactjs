5f83845e359d2a93935a69c1afc85e011da15455
Dayana <dayana@senorcoders.com>
2020-03-16 10:16:02 -0600

    upload project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import { Form, Button } from 'react-bootstrap';
+
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+
+    if(email.length > 0 && password.length > 0){
+      alert("You have logged in successfully");
+    }else{
+      alert("Please fill all the required fields")
+    }
+    
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main-bg">
+    <div className="box shadow">
+      <h2 className="loginHeader">Member Login</h2>
+    <Form className="Login" onSubmit={validateForm}>
+  <Form.Group  controlId="email" >
+    <Form.Control type="text" placeholder="Username" 
+    autoFocus
+    value={email}
+    onChange={e => setEmail(e.target.value)}/>
+ 
+  </Form.Group>
+
+  <Form.Group controlId="password" >
+    <Form.Control type="password" placeholder="Password" 
+    value={password}
+    onChange={e => setPassword(e.target.value)}/>
+  </Form.Group>
+  
+  <Button variant="success" type="submit"  block >
+    Login
+  </Button>
+</Form>
+<div className="forgotPasswordContainer">
+    <a href="#">Forgot Password?</a>
+</div>
+</div>
+</div>
   );
 }
 
